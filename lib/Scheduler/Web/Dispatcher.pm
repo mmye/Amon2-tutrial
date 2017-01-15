2de894b65368dc7b44e41bd0cde7f659284b4fbf
masato <masato@tk2-236-27536.vs.sakura.ne.jp>
2017-01-15 20:27:52 +0900

    First commit

--- lib/Scheduler/Web/Dispatcher.pm
+++ lib/Scheduler/Web/Dispatcher.pm
@@ -4,26 +4,48 @@ use warnings;
 use utf8;
 use Amon2::Web::Dispatcher::RouterBoom;
 
-any '/' => sub {
+use Time::Piece; # (1)
+
+get '/' => sub {
     my ($c) = @_;
-    my $counter = $c->session->get('counter') || 0;
-    $counter++;
-    $c->session->set('counter' => $counter);
-    return $c->render('index.tx', {
-        counter => $counter,
-    });
+    
+    my @schedules = $c->db->search('schedules');
+    return $c->render('index.tx', {schedules => \@schedules });
 };
 
-post '/reset_counter' => sub {
-    my $c = shift;
-    $c->session->remove('counter');
+#登録ボタンの処理
+post '/post' => sub {
+    my ($c) = @_;
+
+    my $title = $c->req->parameters->{title}; #
+    my $date  = $c->req->parameters->{date};  # (2)
+
+    my $date_epoch = Time::Piece->strptime($date, '%Y/%m/%d')->epoch; # (3)
+
+    $c->db->insert(schedules => { #
+        title => $title,          #
+        date  => $date_epoch,     # (4)
+    });                           #
+
     return $c->redirect('/');
 };
 
-post '/account/logout' => sub {
-    my ($c) = @_;
-    $c->session->expire();
+#削除ボタンの処理
+post '/schedules/:id/delete' => sub {
+    my ($c, $args) = @_;
+    my $id = $args->{id};
+
+    $c->db->delete('schedules' => { id => $id });
     return $c->redirect('/');
+};
+
+get '/user' => sub {
+    my ($c) = @_;
+
+    my $name = $c->req->parameters->{name};
+    my $favorite = $c->req->parameters->{favorite};
+    
+    return $c->render('/user');
 };
 
 1;
