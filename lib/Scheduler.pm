f353614a3ad785d00ebc41dfe642593896bc4333
Masato Maeyama <mokoondii2@gmail.com>
2017-06-01 07:41:35 +0900

    Add search function

--- lib/Scheduler.pm
+++ lib/Scheduler.pm
@@ -4,32 +4,35 @@ use warnings;
 use utf8;
 our $VERSION='0.01';
 use 5.008001;
-use Scheduler::DB::Schema;
-use Scheduler::DB;
+use Teng;
+use Teng::Schema::Loader;
 
 use parent qw/Amon2/;
 use File::Spec;
 
 # Enable project local mode.
-__PACKAGE__->make_local_context();
+#__PACKAGE__->make_local_context();
 
-my $schema = Scheduler::DB::Schema->instance;
-
+my $schema;
 sub db {
-    my $c = shift;
-    if (!exists $c->{db}) {
-        my $conf = $c->config->{DBI}
+    my $self = shift;
+    if ( !defined $self->{db} ) {
+        my $conf = $self->config->{DBI}
             or die "Missing configuration about DBI";
-        $c->{db} = Scheduler::DB->new(
-            schema       => $schema,
-            connect_info => [@$conf],
+        my $dbh = $self->dbh;
+        $self->{db} = Teng::Schema::Loader->load(
+            namespace => 'Scheduler::DB',
+            dbh       => $dbh,
+            );
+#        $c->{db} = Scheduler::DB->new(
+#            schema       => $schema,
+#            connect_info => [@$conf],
             # I suggest to enable following lines if you are using mysql.
             # on_connect_do => [
             #     'SET SESSION sql_mode=STRICT_TRANS_TABLES;',
             # ],
-        );
     }
-    $c->{db};
+    return $self->{db};
 }
 
 1;
--- lib/Scheduler/DB.pm
+++ lib/Scheduler/DB.pm
@@ -4,6 +4,6 @@ use warnings;
 use utf8;
 use parent qw/Teng/;
 
-__PACKAGE__->load_plugin('SearchBySQLAbstractMore');
-
+__PACKAGE__->load_plugin('Pager');
+__PACKAGE__->load_plugin('DBI');
 1;
